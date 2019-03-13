1286f2e3f05226fb099fbc2547b8af609ac90c4b
ldomingo2002 <ldominguez@ucvts.org>
2019-03-13 13:22:39 -0400

    typing

--- web.js
+++ web.js
@@ -3,6 +3,16 @@ function startGame(){
 }
 
 
-function sceneOne{
-  
+function typeWriter() {
+  var i = 0;
+  var txt = "Hey";
+  var speed = 1000;
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+
+
+
 }
