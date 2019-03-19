cee0f97c292a5b8756f6a4b5f6fd0953036e69cd
ldomingo2002 <ldominguez@ucvts.org>
2019-03-19 12:44:31 -0400

    WE DID IT

--- web.js
+++ web.js
@@ -4,6 +4,10 @@ document.getElementById('Home').background ="1551412813123.png";
 }
 
 
+function changeImg(){
+document.getElementById('startbutton').style.backgroundImage = "url('Images\ceiling.png')";
+}
+
 //var image = document.getElementById("Home");
 //if(image.src.match("1551412813123.png")){
   //image.src = "Images\1551412813123.png";
