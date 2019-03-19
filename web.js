314aad7a0e71365d83d45d78910fdef2834d053d
ldomingo2002 <ldominguez@ucvts.org>
2019-03-19 12:22:10 -0400

    js

--- web.js
+++ web.js
@@ -4,17 +4,6 @@ document.getElementById('Home').background ="1551412813123.png";
 }
 
 
-function typeWriter() {
-  var i = 0;
-  var txt = "H e y";
-  var speed = 1000;
-
-  if (i < txt.length) {
-    document.getElementById("text").innerHTML + txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
 //var image = document.getElementById("Home");
 //if(image.src.match("1551412813123.png")){
   //image.src = "Images\1551412813123.png";
