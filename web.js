b4a8053fc7e199a93dc654057a90f266c410233f
First Last <youremailaddress@example.com>
2019-03-20 19:26:31 -0400

    next button added on 2nd page but there are 2

--- web.js
+++ web.js
@@ -6,11 +6,3 @@ document.getElementById('canvas').style.background = "url(images/ceiling.png)";
 function changeImg(){
 document.getElementById('startbutton').style.backgroundImage = "url('Images\ceiling.png')";
 }
-
-//var image = document.getElementById("Home");
-//if(image.src.match("1551412813123.png")){
-  //image.src = "Images\1551412813123.png";
-//  } else{
-//    img.src = "Images\1551412813123.png";
-//  }
-//
