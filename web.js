90b81b1dd34eda2e58f589afc812699e1ae2c097
Tunmise Alabi <GIT SERVICE EMAIL>
2019-03-21 12:53:06 -0400

    finally 1 button

--- web.js
+++ web.js
@@ -6,3 +6,14 @@ document.getElementById('canvas').style.background = "url(images/ceiling.png)";
 function changeImg(){
 document.getElementById('startbutton').style.backgroundImage = "url('Images\ceiling.png')";
 }
+
+ function changePage(){
+   var button = document.createElement("button");
+   button.innerHTML = "Next Page";
+
+   // 2. Append somewhere
+   var body = document.getElementsByTagName("body")[0];
+   body.appendChild(button);
+
+
+ }
