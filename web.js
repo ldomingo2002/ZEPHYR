1241c9bae3c959f782d62698b0d79e1e271609cd
Tunmise Alabi <GIT SERVICE EMAIL>
2019-03-21 13:17:24 -0400

    2 option buttons made

--- web.js
+++ web.js
@@ -8,12 +8,21 @@ document.getElementById('startbutton').style.backgroundImage = "url('Images\ceil
 }
 
  function changePage(){
+   //option 1
    var button = document.createElement("button");
-   button.innerHTML = "Next Page";
-
-   // 2. Append somewhere
+   button.innerHTML = "Option 1";
    var body = document.getElementsByTagName("body")[0];
    body.appendChild(button);
+var idT = button;
+idT.setAttribute("id", "nextButton");
+//option 2
+var button = document.createElement("button");
+button.innerHTML = "Option 2";
 
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
 
+var idT = button;
+idT.setAttribute("id", "nextButton2");
  }
