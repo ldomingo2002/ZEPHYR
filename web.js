4e57f882c4b1725fb0da94bf887ff5b15cc82d30
Tunmise Alabi <GIT SERVICE EMAIL>
2019-03-25 13:03:20 -0400

    new page when button clicked

--- web.js
+++ web.js
@@ -6,11 +6,21 @@ function myFunction() {
     document.getElementById("disapear").style.display = "none";
   }
 
+  function losePage1() {
+    document.body.style.backgroundImage = "url('images/creepyhallway.png')";
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundPosition = "top right";
+    document.getElementById("nextButton").style.display = "none";
+    document.getElementById("nextButton2").style.display = "none";
+
+  }
+
  function changePage(){
    //option 1
    var button1 = document.createElement("button");
    button1.innerHTML = "Option 1";
-
+   //no option option
+button1.onclick = losePage1;
    var body = document.getElementsByTagName("body")[0];
    body.appendChild(button1);
 var idT = button1;
