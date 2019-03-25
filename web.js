edc3a082bfe87fd495c1b0493e5382e252715fa6
Tunmise Alabi <GIT SERVICE EMAIL>
2019-03-25 12:45:53 -0400

    change page function added to js page

--- web.js
+++ web.js
@@ -1,24 +1,28 @@
-function startGame(){
-document.getElementById('canvas').style.background = "url(images/ceiling.png)";
-}
-
+//to change pages
+function myFunction() {
+    document.body.style.backgroundImage = "url('Images/leceiling.png')";
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundPosition = "top right";
+    document.getElementById("disapear").style.display = "none";
+  }
 
  function changePage(){
    //option 1
-   var button = document.createElement("button");
-   button.innerHTML = "Option 1";
+   var button1 = document.createElement("button");
+   button1.innerHTML = "Option 1";
+
    var body = document.getElementsByTagName("body")[0];
-   body.appendChild(button);
-var idT = button;
+   body.appendChild(button1);
+var idT = button1;
 idT.setAttribute("id", "nextButton");
+
 //option 2
-var button = document.createElement("button");
-button.innerHTML = "Option 2";
+var button2 = document.createElement("button");
+button2.innerHTML = "Option 2";
 
-// 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
+body.appendChild(button2);
 
-var idT = button;
+var idT = button2;
 idT.setAttribute("id", "nextButton2");
  }
